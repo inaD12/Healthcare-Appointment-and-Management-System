d7e47d7cf13e00b799ec854774b78aa6201d1e38
Йордан Стойчев <capitanblack747@gmail.com>
2025-01-28 19:03:05 +0200

    SharedTestUtilities

---
+++ src/Contracts/Contracts/Utilities/SharedTestUtilities.cs
@@ -0,0 +1,51 @@
+﻿using Bogus;
+
+namespace Contracts.Utilities;
+
+public class SharedTestUtilities
+{
+	private static readonly Faker _faker = new();
+
+	private const int DefaultStringLength = 30;
+	public static string GetGuid()
+	{
+		var result = _faker.Random.Guid().ToString();
+
+		return result;
+	}
+
+	public static string GetString(int length = DefaultStringLength)
+	{
+		var result = _faker.Random.AlphaNumeric(length);
+
+		return result;
+	}
+
+	public static string GetAverageString(int maxLength, int minLength)
+	{
+		var result = _faker.Random.AlphaNumeric(GetAverageNumber(maxLength, minLength));
+
+		return result;
+	}
+
+	public static int GetAverageNumber(int maxLength, int minLength)
+	{
+		var result = (maxLength + minLength) / 2;
+
+		return result;
+	}
+
+	public static string GetHalfStartString(string value)
+	{
+		var result = value[..(value.Length / 2)];
+
+		return result;
+	}
+
+	public static string GetNonCaseMatchingString(string value)
+	{
+		var result = value.ToUpper();
+
+		return result;
+	}
+}
