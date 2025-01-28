2b58a143467eee2c28e9f9f34941287020598ed1
Йордан Стойчев <capitanblack747@gmail.com>
2025-01-28 19:39:30 +0200

    Appointments and Users test utilities

---
+++ src/Appointments/Appointments.Domain/Utilities/AppointmentsTestUtilities.cs
@@ -0,0 +1,12 @@
+﻿using Contracts.Utilities;
+
+namespace Appointments.Domain.Utilities;
+
+public class AppointmentsTestUtilities : SharedTestUtilities
+{
+	public static readonly string ValidId = GetAverageString(AppointmentsBusinessConfiguration.ID_MAX_LENGTH, AppointmentsBusinessConfiguration.ID_MIN_LENGTH);
+	public static readonly string InvalidId = GetAverageString(AppointmentsBusinessConfiguration.ID_MAX_LENGTH, AppointmentsBusinessConfiguration.ID_MIN_LENGTH);
+
+	public static readonly string PatientId = GetAverageString(AppointmentsBusinessConfiguration.PATIENTID_MIN_LENGTH, AppointmentsBusinessConfiguration.PATIENTID_MAX_LENGTH);
+	public static readonly string DoctorId = GetAverageString(AppointmentsBusinessConfiguration.DOCTORID_MIN_LENGTH, AppointmentsBusinessConfiguration.DOCTORID_MAX_LENGTH);
+}
---
+++ src/Users/Users.Domain/Utilities/UsersTestUtilities.cs
@@ -0,0 +1,16 @@
+﻿using Contracts.Utilities;
+
+namespace Users.Domain.Utilities;
+
+public class UsersTestUtilities : SharedTestUtilities
+{
+	public static readonly string ValidId = GetAverageString(UsersBusinessConfiguration.ID_MAX_LENGTH, UsersBusinessConfiguration.ID_MIN_LENGTH);
+	public static readonly string InvalidId = GetAverageString(UsersBusinessConfiguration.ID_MAX_LENGTH, UsersBusinessConfiguration.ID_MIN_LENGTH);
+
+	public static readonly string ValidFirstName = GetAverageString(UsersBusinessConfiguration.FIRSTNAME_MIN_LENGTH, UsersBusinessConfiguration.FIRSTNAME_MAX_LENGTH);
+	public static readonly string ValidLastName = GetAverageString(UsersBusinessConfiguration.LASTTNAME_MIN_LENGTH, UsersBusinessConfiguration.LASTNAME_MAX_LENGTH);
+
+	public static readonly string ValidPassword = GetAverageString(UsersBusinessConfiguration.PASSWORD_MIN_LENGTH, UsersBusinessConfiguration.PASSWORD_MAX_LENGTH);
+	public static readonly string InvalidPassword = GetAverageString(UsersBusinessConfiguration.PASSWORD_MIN_LENGTH, UsersBusinessConfiguration.PASSWORD_MAX_LENGTH);
+}
+}
