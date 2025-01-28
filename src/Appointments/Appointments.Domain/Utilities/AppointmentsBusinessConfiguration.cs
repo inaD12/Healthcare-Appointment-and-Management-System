f14a6a7309f8a9240408643fd86d14f6a7737ba3
Йордан Стойчев <capitanblack747@gmail.com>
2025-01-28 18:50:25 +0200

    Business configuration for Appointments and Users

---
+++ src/Appointments/Appointments.Domain/Utilities/AppointmentsBusinessConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace Appointments.Domain.Utilities;
+
+public class AppointmentsBusinessConfiguration
+{
+	public const int PATIENTID_MIN_LENGTH = 7;
+	public const int PATIENTID_MAX_LENGTH = 100;
+
+	public const int DOCTORID_MIN_LENGTH = 7;
+	public const int DOCTORID_MAX_LENGTH = 100;
+}
---
+++ src/Users/Users.Domain/Utilities/UsersBusinessConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace Users.Domain.Utilities;
+
+public class UsersBusinessConfiguration
+{
+	public const int ID_MIN_LENGTH = 7;
+	public const int ID_MAX_LENGTH = 100;
+
+	public const int FIRSTNAME_MIN_LENGTH = 3;
+	public const int FIRSTNAME_MAX_LENGTH = 30;
+
+	public const int LASTTNAME_MIN_LENGTH = 3;
+	public const int LASTNAME_MAX_LENGTH = 30;
+}
