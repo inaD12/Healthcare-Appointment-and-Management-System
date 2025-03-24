15b5171cc5f4ad2e151bc045b2ef9bb8eb735c6f
Йордан Стойчев <capitanblack747@gmail.com>
2025-03-24 15:18:16 +0200

    GetAll requests refactored to records

--- src/Appointments/Appointments.API/Appointments/Models/Requests/GetAllAppointmentsRequest.cs
+++ src/Appointments/Appointments.API/Appointments/Models/Requests/GetAllAppointmentsRequest.cs
@@ -1,13 +1,19 @@
 ﻿using Appointments.Domain.Entities.Enums;
 using Shared.API.Models.Requests;
+using Shared.Domain.Enums;
 
 namespace Appointments.API.Appointments.Models.Requests;
 
-public class GetAllAppointmentsRequest : CollectionReadRequest
-{
-	public string? PatientId { get; set; } = string.Empty;
-	public string? DoctorId { get; set; } = string.Empty;
-	public AppointmentStatus? Status { get; set; }
-	public DateTime? FromTime { get; set; }
-	public DateTime? ToTime { get; set; }
-}
+public record GetAllAppointmentsRequest
+(
+	string PatientId = "",
+	string DoctorId = "",
+	AppointmentStatus? Status = null,
+	DateTime? FromTime = null,
+	DateTime? ToTime = null,
+	SortOrder SortOrder = SortOrder.ASC,
+	string SortPropertyName = "Id",
+	int Page = 1,
+	int PageSize = 10
+) : CollectionReadRequest(SortOrder, SortPropertyName, Page, PageSize);
+
