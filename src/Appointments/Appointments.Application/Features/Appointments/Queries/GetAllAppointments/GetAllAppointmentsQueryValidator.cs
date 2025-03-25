c9ca99666816e8647467e4f3aaba90f4cea96c54
Йордан Стойчев <capitanblack747@gmail.com>
2025-03-25 19:40:54 +0200

    GetAllAppointmentsQueryHandler and GetAllAppointmentsQueryValidator unit tests

--- src/Appointments/Appointments.Application/Features/Appointments/Queries/GetAllAppointments/GetAllAppointmentsQueryValidator.cs
+++ src/Appointments/Appointments.Application/Features/Appointments/Queries/GetAllAppointments/GetAllAppointmentsQueryValidator.cs
@@ -1,4 +1,5 @@
 ﻿using Appointments.Domain.Entities;
+using Appointments.Domain.Utilities;
 using FluentValidation;
 
 namespace Appointments.Application.Features.Appointments.Queries.GetAppointmentsUsers;
@@ -7,6 +8,28 @@ public class GetAllAppointmentsQueryValidator : AbstractValidator<GetAllAppointm
 {
 	public GetAllAppointmentsQueryValidator()
 	{
+		RuleFor(q => q.PatientId)
+		   .MinimumLength(AppointmentsBusinessConfiguration.ID_MIN_LENGTH)
+		   .MaximumLength(AppointmentsBusinessConfiguration.ID_MAX_LENGTH)
+		   .When(q => !string.IsNullOrEmpty(q.PatientId));
+
+		RuleFor(q => q.DoctorId)
+		   .MinimumLength(AppointmentsBusinessConfiguration.ID_MIN_LENGTH)
+		   .MaximumLength(AppointmentsBusinessConfiguration.ID_MAX_LENGTH)
+		   .When(q => !string.IsNullOrEmpty(q.PatientId));
+
+		RuleFor(q => q.Status)
+			.IsInEnum()
+			.When(q => q.Status.HasValue);
+
+		RuleFor(q => q.FromTime)
+			.LessThanOrEqualTo(q => q.ToTime)
+			.When(q => q.FromTime.HasValue && q.ToTime.HasValue);
+
+		RuleFor(q => q.ToTime)
+			.GreaterThanOrEqualTo(q => q.FromTime)
+			.When(q => q.FromTime.HasValue && q.ToTime.HasValue);
+
 		RuleFor(q => q.SortPropertyName)
 			.Must(BeAValidSortProperty).WithMessage("SortPropertyName must be a valid property");
 	}
