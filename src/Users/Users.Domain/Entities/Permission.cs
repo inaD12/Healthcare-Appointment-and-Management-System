5aef518a88899b612dfe91f7d98aee7985a487fa
inaD12 <capitanblack747@gmail.com>
2025-09-25 12:58:02 +0300

    feature/RBAC: Roles and permissions

---
+++ src/Users/Users.Domain/Entities/Permission.cs
@@ -0,0 +1,20 @@
+﻿namespace Users.Domain.Entities;
+
+public sealed class Permission
+{
+    public static readonly Permission GetUser = new("users:read");
+    public static readonly Permission ModifyUser = new("users:update");
+    public static readonly Permission DeleteUser = new("users:delete");
+    
+    public static readonly Permission CreateAppointment = new("appointment:create");
+    public static readonly Permission CancelAppointment = new("appointment:cancel");
+    public static readonly Permission RescheduleAppointment = new("appointment:reschedule");
+    public static readonly Permission GetAppointment = new("appointment:read");
+
+    public Permission(string code)
+    {
+        Code = code;
+    }
+
+    public string Code { get; }
+}
---
+++ src/Users/Users.Domain/Entities/Role.cs
@@ -0,0 +1,19 @@
+﻿namespace Users.Domain.Entities;
+
+public sealed class Role
+{
+    public static readonly Role Administrator = new("Administrator");
+    public static readonly Role Patient = new("Patient");
+    public static readonly Role Doctor = new("Doctor");
+
+    private Role(string name)
+    {
+        Name = name;
+    }
+
+    private Role()
+    {
+    }
+
+    public string Name { get; private set; }
+}
