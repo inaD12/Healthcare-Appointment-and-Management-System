79f0132dddc9a251df3ea7567137b11dcb6a4b87
inaD12 <danstoy@abv.bg>
2025-10-11 20:05:31 +0300

    feature/doctors-service: add DoctorsDbContext

---
+++ src/Doctors/Doctors.Infrastructure/Features/Configurations/SpecialityConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Doctors.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Doctors.Infrastructure.Features.Configurations;
+
+internal class SpecialityConfiguration : IEntityTypeConfiguration<Speciality>
+{
+	public void Configure(EntityTypeBuilder<Speciality> builder)
+	{
+        builder.HasIndex(s => s.Name).IsUnique();
+	}
+}
