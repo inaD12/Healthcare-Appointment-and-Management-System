2c1ac8a28b2f93ec3aefea8e201c6f8c951986db
inaD12 <danstoy@abv.bg>
2026-01-25 18:12:31 +0200

    features/ratings: add dbcontext and configurations

---
+++ src/Ratings/Ratings.Infrastructure/Features/DBContexts/RatingsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ratings.Domain.Entities;
+using Ratings.Infrastructure.Features.Configurations;
+
+namespace Ratings.Infrastructure.Features.DBContexts;
+
+public sealed class RatingsDbContext(DbContextOptions<RatingsDbContext> options) : DbContext(options)
+{
+	public DbSet<Rating> Ratings => Set<Rating>();
+	public DbSet<DoctorRatingStats> DoctorRatingStats => Set<DoctorRatingStats>();
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.ApplyConfiguration(new RatingConfiguration());
+		modelBuilder.ApplyConfiguration(new DoctorRatingStatsConfiguration());
+	}
+}
