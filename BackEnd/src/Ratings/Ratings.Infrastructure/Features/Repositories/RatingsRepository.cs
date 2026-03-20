07b922c9889935bf6a58f0ed266c9cf48dde0ced
inaD12 <danstoy@abv.bg>
2026-03-20 13:36:23 +0200

    features/get-all-ratings: add GetAll method to repository

--- BackEnd/src/Ratings/Ratings.Infrastructure/Features/Repositories/RatingsRepository.cs
+++ BackEnd/src/Ratings/Ratings.Infrastructure/Features/Repositories/RatingsRepository.cs
@@ -1,7 +1,10 @@
 using System.Data.Entity;
 using Ratings.Domain.Abstractions.Repositories;
 using Ratings.Domain.Entities;
+using Ratings.Domain.Models;
 using Ratings.Infrastructure.Features.DBContexts;
+using Shared.Domain.Models;
+using Shared.Infrastructure.Extensions;
 using Shared.Infrastructure.Repositories;
 
 namespace Ratings.Infrastructure.Features.Repositories;
@@ -16,5 +19,35 @@ public class RatingsRepository(RatingsDbContext context)
         return await context.Ratings
             .AsNoTracking()
             .AnyAsync(r => r.AppointmentId == appointmentId, cancellationToken);
+    }
+    
+    public async Task<PagedList<Rating>> GetAllAsync(
+        RatingPagedListQuery query,
+        CancellationToken cancellationToken = default)
+    {
+        var entitiesQuery = context.Ratings
+            .Where(r =>
+                (string.IsNullOrEmpty(query.DoctorId) || r.DoctorId == query.DoctorId) &&
+                (string.IsNullOrEmpty(query.PatientId) || r.PatientId == query.PatientId) &&
+                (string.IsNullOrEmpty(query.AppointmentId) || r.AppointmentId == query.AppointmentId) &&
+                (!query.MinScore.HasValue || r.Score >= query.MinScore.Value) &&
+                (!query.MaxScore.HasValue || r.Score <= query.MaxScore.Value)
+            );
+
+        if (string.IsNullOrEmpty(query.SortPropertyName))
+        {
+            entitiesQuery = entitiesQuery.OrderByDescending(r => r.CreatedAt);
+        }
+        else
+        {
+            entitiesQuery = entitiesQuery.ApplySorting(query.SortPropertyName, query.SortOrder);
+        }
+
+        return await PagedList<Rating>.CreateAsync(
+            entitiesQuery,
+            query.Page,
+            query.PageSize,
+            cancellationToken
+        );
     }
 }
