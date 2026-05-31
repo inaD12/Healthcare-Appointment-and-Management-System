7ce8538144be9c4796b38dc8ff177a826c643b96
inaD12 <danstoy@abv.bg>
2026-05-31 19:49:21 +0300

    features/name-update: add name update consumer in patients microservice

---
+++ BackEnd/src/Patients/Patients.Infrastructure/Features/Consumers/UserUpdatedNamesIntegrationEventConsumer.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+using Patients.Domain.Abstractions.Repositories.Command;
+using Serilog;
+using Shared.Application.IntegrationEvents;
+using Shared.Domain.Abstractions;
+
+namespace Patients.Infrastructure.Features.Consumers;
+
+public sealed class UserUpdatedNamesIntegrationEventConsumer(
+    IPatientCommandRepository repository,
+    IUnitOfWork unitOfWork)
+    : IConsumer<UserUpdatedNamesIntegrationEvent>
+{
+    public async Task Consume(ConsumeContext<UserUpdatedNamesIntegrationEvent> context)
+    {
+        var msg = context.Message;
+        
+        var patient = await repository.GetByIdAsync(msg.UserId, context.CancellationToken);
+        if (patient is null)
+        {
+            Log.Error($"Name update failed for user {msg.UserId} in UserUpdatedNamesIntegrationEventConsumer.");
+            return;
+        }
+        
+        patient.ChangeNames(msg.FirstName, msg.LastName);
+        await unitOfWork.SaveChangesAsync(context.CancellationToken);
+    }
+}
