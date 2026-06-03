8c8f3040f7df1d9ce56dd12ccdd213c757723cdc
inaD12 <danstoy@abv.bg>
2026-06-03 16:29:42 +0300

    feature/deleted-cascade: add integration event

---
+++ BackEnd/src/Shared/Shared.Application/IntegrationEvents/UserDeletedIntegrationEvent.cs
@@ -0,0 +1,3 @@
+﻿namespace Shared.Application.IntegrationEvents;
+
+public record UserDeletedIntegrationEvent(string Id);
---
+++ BackEnd/src/Users/Users.Infrastructure/Features/Consumers/UserDeletedDomainEventConsumer.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using Shared.Application.Abstractions;
+using Shared.Application.IntegrationEvents;
+using Users.Domain.Events;
+
+namespace Users.Infrastructure.Features.Consumers;
+
+public sealed class UserDeletedDomainEventConsumer(
+	IEventBus eventBus)
+	: IConsumer<UserDeletedDomainEvent>
+{
+	public async Task Consume(ConsumeContext<UserDeletedDomainEvent> context)
+	{
+		var msg = context.Message;
+
+		var userDeletedEvent = new UserDeletedIntegrationEvent(msg.Id);
+
+		await eventBus.PublishAsync(userDeletedEvent, context.CancellationToken);
+	}
+}
